65134ac9880c5a6aa919c9fb12ca1894b832dfda
atcastells <aron.deltebre@gmail.com>
2017-03-01 20:29:55 +0100

    Afegit Estil css de la pagina anterior
    Modificada la vista "welcome" per incloure la master
    
    Afegits enllaços a la master de jquery i css

--- resources/views/partials/master.blade.php
+++ resources/views/partials/master.blade.php
@@ -3,6 +3,8 @@
 <head>
 	<meta charset="UTF-8">
 	<title>@yield('titol')</title>
+	{!! Html::script('js/jquery-3.1.1.min.js') !!}
+    {!! Html::style('css/estil.css') !!}
 </head>
 <body>
 	@yield('contingut')
