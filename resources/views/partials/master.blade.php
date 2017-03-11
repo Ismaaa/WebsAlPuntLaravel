e611de020872556f37a39726bf371a3c23824c98
Ismaaa <didouhismael@gmail.com>
2017-03-11 13:31:38 +0100

    asd

--- resources/views/partials/master.blade.php
+++ resources/views/partials/master.blade.php
@@ -1,32 +1,9 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-<<<<<<< HEAD
 	<meta charset="UTF-8">
 	<title>@yield('titol')</title>
 	{!! Html::script('js/jquery-3.1.1.min.js') !!}
-=======
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-	<!-- CSRF Token -->
-	<meta name="csrf-token" content="{{ csrf_token() }}">
-
-	<title>{{ config('app.name', 'Laravel') }}</title>
-
-	<!-- Styles -->
-	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
-	<!-- Scripts -->
-	<script>
-			window.Laravel = {!! json_encode([
-					'csrfToken' => csrf_token(),
-			]) !!};
-	</script>
-
-	  {!! Html::script('js/jquery-3.1.1.min.js') !!}
->>>>>>> b4cb0efe37e0c3148ed6461d4369d1c3a289e6f6
     {!! Html::style('css/estil.css') !!}
     <!-- Styles -->
     <link href="{{ asset('plugins/sweetalert/dist/sweetalert.css') }}" rel="stylesheet">
