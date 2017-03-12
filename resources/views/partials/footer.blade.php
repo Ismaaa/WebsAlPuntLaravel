0f66824c173af7d5ad85d22ada91bfe54b7e5d1d
Joan <joanbarrera@iesmontsia.org>
2017-03-12 14:26:30 +0100

    Important styles Estructura recepta modificat Logos modificats Footers modificats

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,40 @@
+<link media="all" type="text/css" rel="stylesheet" href="/css/estil.css">
+<!--
+<div id="footer">
+    <div class="container">
+        <div class="row">
+            <div class="grid_12">
+                <div class="socials">
+                    <a href="http://www.dinahosting.com" target="_blank"><img src="/img/dinahosting.jpg"></a>
+                    <a href="http://websalpunt.cat/" target="_blank" ><img src="/img/puntcat.png"/></a>
+                </div>
+            </div>
+        </div>
+        <div class="rowCopyright">
+            <div class="copyright">
+                <span class="brand">Unitedcode </span> &copy; <span id="copyright-year"></span> | <a href="#">Política de privacitat</a>
+                <div>Lloc dissenyat per <a href="#" rel="nofollow">United Code</a></div>
+                <div></div><a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
+                    <img alt="Llicència de Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/80x15.png" />
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+-->
+<div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <a href="http://websalpunt.cat/" target="_blank" ><img align="right" class="footer-img" src="/img/puntcat.png"/></a>
+                <a href="http://www.dinahosting.com" target="_blank"><img align="right" class="footer-img" src="/img/dinahosting.jpg"></a>
+
+                <span>Unitedcode </span> &copy; | <a href="#"><b>Política de privacitat</b></a>
+                <p>Lloc dissenyat per <a href="#"><b>United Code</b></a></p>
+                <a target="_blank" rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
+                    <img alt="Llicència de Creative Commons" src="https://i.creativecommons.org/l/by-nc-sa/4.0/80x15.png" />
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
